91f7bddc648c28e659880e0ae9a27b563ff135ce
Marco Afonso <mafonso333@gmail.com>
2013-10-12 13:40:48 +0100

    Improvements on default View class

--- lib/View.php
+++ lib/View.php
@@ -1,65 +1,125 @@
 <?php
 
+/**
+ * View class
+ */
 class View {
     
-    public $data = array();
-    public $slot = array('content' => array());
+    protected $data = array();
+    protected $slot = array('content' => array());
     protected $path;
-    protected $out;
+    protected $output;
 
+    /**
+     * Constructor
+     * 
+     * @param mixed $mixed
+     * @param array $data
+     */
     public function __construct($mixed = '', $data = array()) {
 
         // check if $mixed is a file path
         if (file_exists($mixed)) $this->path = $mixed;
 
         // if not, consider it as content
-        else $this->out = $mixed;
+        else $this->output = $mixed;
         
         // set default data
         $this->data = $data;
     }
     
+    /**
+     * Calls a slot using a template callback
+     * 
+     * @param string $name
+     * @param function $template
+     * @return \View
+     */
     function slot($name, $template) {
-        if (empty($this->slot[$name])) return;
-        foreach ($this->slot[$name] as $item) {
-            $template($item);
+        if (!empty($this->slot[$name])) {
+            foreach ($this->slot[$name] as $item) {
+                $template($item);
+            }
         }
+        return $this;
     }
+    
+    /**
+     * Sets up a new slot identified by name
+     * 
+     * @param string $name
+     * @return \View
+     */
     public function addSlot($name) {
         $this->slot[$name] = array();
         return $this;
     }
-    public function addContent($item, $slotName = 'content', $unique = false) {
+    
+    /**
+     * Adds content to the view
+     * This can be a string, a View or a php file template path
+     * 
+     * You can add unique content by setting $unique to true. This will check
+     * whether the content is already on the view or not
+     * 
+     * @param mixed $content
+     * @param string $slotName
+     * @param boolean $unique
+     * @return \View
+     */
+    public function addContent($content, $slotName = 'content', $unique = false) {
         $skip = false;
         if (in_array($slotName, array('css', 'js'))) $unique = true;
         if ($unique) {
             foreach ($this->slot[$slotName] as $titem) {
-                if (gettype($titem) == gettype($item) && $titem == $item) $skip = true;
+                if (gettype($titem) == gettype($content) && $titem == $content) $skip = true;
             }
         }
-        if (!$skip) $this->slot[$slotName][] = $item;
+        if (!$skip) $this->slot[$slotName][] = $content;
         return $this;
     }
+    
+    /**
+     * Set data to the view
+     * This data keys will be transform to $variables using explode()
+     * The value can be anything ie. string, array, object, etc...
+     * Just make sure it will be properly used in the template
+     * 
+     * @param string $key
+     * @param mixed $value
+     * @return \View
+     */
     public function set($key, $value) {
         $this->data[$key] = $value;
         return $this;
     }
+    
+    /**
+     * Returns the data value identified by key
+     * 
+     * @param string $key
+     * @return mixed
+     */
     public function get($key) {
         return $this->data[$key];
     }
+    
+    /**
+     * Sets the path template that will be used
+     * @param string $path
+     * @return \View
+     */
     public function setPath($path) {
         $this->path = $path;
         return $this;
     }
+    
     public function __toString() {
-        if (!empty($this->out)) return $this->out;
+        if (!file_exists($this->path)) return $this->output;
         ob_start();
         extract($this->data);
-        if (file_exists($this->path)) require_once $this->path;
-        else {
-            foreach ($this->slot['content'] as $item) echo $item;
-        }
-        $this->out = (string) ob_get_clean();
-        return $this->out;
+        include $this->path;
+        $this->output = (string) ob_get_clean();
+        return $this->output;
     }
 }
