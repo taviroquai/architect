c10146ff8ee102dc1f228849b21ac78991d7ef86
Marco Afonso <mafonso333@gmail.com>
2013-11-01 01:44:56 +0000

    Manually include aliases.php

--- tests/index.php
+++ tests/index.php
@@ -1,11 +1,12 @@
 <?php
 
 // Autoload files using Composer autoload
-require_once realpath( __DIR__ . '/../') . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../aliases.php';
 
 // Create application
 $config_path = realpath('config/config.xml');
-$app = \Arch\App::Instance($config_path)->aliases();
+$app = \Arch\App::Instance($config_path);
 
 // Add default route
 $app->addRoute('/', function () use ($app) {
