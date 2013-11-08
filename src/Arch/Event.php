d81dfed9fd88ffe360b6005b2b9b1361e77c3206
Marco Afonso <mafonso333@gmail.com>
2013-11-08 19:58:03 +0000

    Set optional target for \Arch\Event

--- src/Arch/Event.php
+++ src/Arch/Event.php
@@ -7,20 +7,36 @@ namespace Arch;
  */
 class Event
 {
-
-    protected $name;
-    protected $callback;
-    protected $target;
     
+    /**
+     * The event name
+     * @var string
+     */
+    protected $name;
+    
+    /**
+     * The event callback
+     * @var mixed
+     */
+    protected $callback;
+    
+    /**
+     * Returns a new event
+     * @param string $name The event name
+     * @param mixed $callback The event callback
+     */
     public function __construct($name, $callback)
     {
         $this->name = $name;
         $this->callback = $callback;
     }
     
+    /**
+     * Triggers this event
+     * @param mixed $target An optional target
+     */
     public function trigger($target = null)
     {
-        $target = empty($target) ? $this->target : $target;
         $fn = $this->callback;
         if (is_callable($fn)) {
             $fn($target);
