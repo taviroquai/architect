26d5ed220fe147976fcf410b4a531155c3018b40
Marco Afonso <mafonso333@gmail.com>
2013-11-09 13:02:26 +0000

    Add comments to message

--- src/Arch/Message.php
+++ src/Arch/Message.php
@@ -6,10 +6,25 @@ namespace Arch;
  * Class message
  */
 class Message
-{	
+{
+    
+    /**
+     * Hold the message text
+     * @var string
+     */
 	public $text;
+    
+    /**
+     * Holds the HTML element class attribute to be used in template
+     * @var string
+     */
 	public $cssClass;
 
+    /**
+     * Returns a new message
+     * @param type $text The message text
+     * @param type $cssClass The HTML element class attribute
+     */
 	public function __construct($text, $cssClass = 'alert alert-success')
     {
 		$this->text = $text;
