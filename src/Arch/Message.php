4a8063a8cb88a61c84cf3729b385be5e79e951ce
Marco Afonso <mafonso333@gmail.com>
2013-11-24 23:29:12 +0000

    Fix Message class

--- src/Arch/Message.php
+++ src/Arch/Message.php
@@ -12,25 +12,25 @@ class Message
      * Hold the message text
      * @var string
      */
-	public $text;
+    public $text;
     
     /**
      * Holds the HTML element class attribute to be used in template
      * @var string
      */
-	public $cssClass;
+    public $cssClass;
 
     /**
      * Returns a new message
      * @param string $text The message text
      * @param string $cssClass The HTML element class attribute
      */
-	public function __construct($text, $cssClass = 'alert alert-success')
+    public function __construct($text, $cssClass = 'alert alert-success')
     {
         if  (!is_string($text) || empty($text)) {
-            throw new \Exception('Invalid filename');
+            throw new \Exception('Invalid message');
         }
-		$this->text = $text;
-		$this->cssClass = $cssClass;
-	}
+        $this->text = $text;
+        $this->cssClass = $cssClass;
+    }
 }
