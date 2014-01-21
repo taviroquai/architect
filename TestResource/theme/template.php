919508e012a0922141775c6d44cea68f964a6715
Marco Afonso <mafonso333@gmail.com>
2014-01-21 23:40:57 +0000

    Fix test template

--- TestResource/theme/template.php
+++ TestResource/theme/template.php
@@ -6,18 +6,18 @@
         
         <?php $this->render('css', function($item) { ?>
         <link href="<?php echo $item; ?>" rel="stylesheet" />
-        <?})?>
+        <?php }) ?>
         
         <?php $this->render('js', function($item) { ?>
         <script src="<?php echo $item; ?>" type="text/javascript"></script>
-        <?})?>
+        <?php }) ?>
         
     </head>
     <body>
         
         <?php $this->render('content', function($item) { ?>
         <div><?php echo $item; ?></div>
-        <?})?>
+        <?php }) ?>
         
     </body>
 </html>
