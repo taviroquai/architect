fc22034f52826d72deebfe117db93ebd52b23853
Marco Afonso <mafonso333@gmail.com>
2014-01-26 14:19:33 +0000

    Reduce \Arch\Theme\Directory load method complexity

--- src/Arch/Theme/Directory.php
+++ src/Arch/Theme/Directory.php
@@ -20,20 +20,15 @@ class Directory extends \Arch\ITheme
      */
     public function load($path)
     {
-        if (!is_dir($path)) {
-            throw new \Exception('Theme directory not found: '.$path);
-        }
-        
-        $template = $path.DIRECTORY_SEPARATOR.'template.php';
-        if (!is_file($template)) {
-            throw new \Exception('Theme template not found: '.$template);
-        }
+        // validate directory
+        $this->validateDirectory($path);
         
         // clean current data
         parent::__construct();
         $this->theme_path = $path;
         
         // set required template
+        $template = $path.DIRECTORY_SEPARATOR.'template.php';
         $this->setTemplate($template);
         
         // add default theme slots
@@ -42,19 +37,44 @@ class Directory extends \Arch\ITheme
         // load slots configuration
         $filename = $this->theme_path.DIRECTORY_SEPARATOR.'slots.xml';
         if (file_exists($filename)) {
-            $xml = @simplexml_load_file($filename);
-            foreach ($xml->slot as $slot) {
-                $slotName = (string) $slot['name'];
-                $this->addSlot($slotName);
-                foreach ($slot->module as $item) {
-                    $classname = (string) $item->classname;
-                    if (!class_exists($classname)) {
-                        continue;
-                    }
-                    $c = isset($item->content) ? (string) $item->content : '';
-                    $module = new $classname($c);
-                    $this->addContent($module, $slotName);
+            $this->loadSlots($filename);
+        }
+    }
+    
+    /**
+     * Validates theme directory
+     * @param string $path The theme directory
+     * @throws \Exception
+     */
+    protected function validateDirectory($path)
+    {
+        if (!is_dir($path)) {
+            throw new \Exception('Theme directory not found: '.$path);
+        }
+        
+        if (!is_file($path.DIRECTORY_SEPARATOR.'template.php')) {
+            throw new \Exception('Theme template not found at '.$path);
+        }
+    }
+    
+    /**
+     * Loads slots configuration
+     * @param string $filename The filename with slots configuration
+     */
+    protected function loadSlots($filename)
+    {
+        $xml = @simplexml_load_file($filename);
+        foreach ($xml->slot as $slot) {
+            $slotName = (string) $slot['name'];
+            $this->addSlot($slotName);
+            foreach ($slot->module as $item) {
+                $classname = (string) $item->classname;
+                if (!class_exists($classname)) {
+                    continue;
                 }
+                $c = isset($item->content) ? (string) $item->content : '';
+                $module = new $classname($c);
+                $this->addContent($module, $slotName);
             }
         }
     }
